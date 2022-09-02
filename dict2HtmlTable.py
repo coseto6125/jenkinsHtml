1ca85009ad92eb7b1b65c6c8ce2a051ef58cd45a
coseto6125 <80243681+coseto6125@users.noreply.github.com>
2022-09-03 02:43:12 +0800

    Update dict2HtmlTable.py

--- dict2HtmlTable.py
+++ dict2HtmlTable.py
@@ -2,13 +2,14 @@
 # @Author: E-NoR
 # @Date:   2022-09-02 23:58:53
 # @Last Modified by:   E-NoR
-# @Last Modified time: 2022-09-03 02:40:30
+# @Last Modified time: 2022-09-03 02:42:43
 
-from html import unescape 
+from html import unescape
 from datetime import timezone, datetime
 
+
 class HTML:
-    def __init__(self, Header, tableStyles = {}, trStyles = {}, thStyles = {}):
+    def __init__(self, Header, tableStyles={}, trStyles={}, thStyles={}):
         trStyles = HTML._styleConverter(trStyles)
         thStyles = HTML._styleConverter(thStyles)
         self.rows = []
@@ -17,23 +18,23 @@ class HTML:
         self.tableStyles = HTML._styleConverter(tableStyles)
 
     @staticmethod
-    def _styleConverter(styleDict : dict):
+    def _styleConverter(styleDict: dict):
         if styleDict == {}:
             return ''
-        styles = ''.join(f'{style}: {value};' for [style, value] in styleDict.items())
+        styles = ''.join(f'{style}: {value};' for [
+                         style, value] in styleDict.items())
         return f'style="{styles}"'
 
-    def addRow(self, row, trStyles = {}, tdStyles = {}):
+    def addRow(self, row, trStyles={}, tdStyles={}):
         trStyles = HTML._styleConverter(trStyles)
         tdStyles = HTML._styleConverter(tdStyles)
         temp_row = f'\n<tr {trStyles} >'
-        for idx,td in enumerate(row):
+        for idx, td in enumerate(row):
             if idx == 6:
                 td = f'<a href="{td}">click</a>'
             temp_row += f'\n<td {tdStyles} >{td}</td>'
         temp_row += '\n</tr>'
         self.rows.append(temp_row)
-
 
     def __str__(self):
         tFormat = "%Y-%m-%d %I:%M:%S %p"
@@ -41,41 +42,45 @@ class HTML:
         utcTime = theTime.astimezone(timezone.utc).strftime(tFormat)
         gmt8 = datetime.strftime(theTime, tFormat)
         content = (
-        f'<h1 style="text-align: center;"> [{gmt8} -- GMT+8]    [{utcTime} -- utc]</h1>\n'
-        f'<table {self.tableStyles}>\n'
-        f'{self.header}'
-        f'{"".join(self.rows)}'
-        '</table>'
+            f'<h1 style="text-align: center;"> [{gmt8} -- GMT+8]    [{utcTime} -- utc]</h1>\n'
+            f'<table {self.tableStyles}>\n'
+            f'{self.header}'
+            f'{"".join(self.rows)}'
+            '</table>'
         )
         return content
 
-def dictionaryToHTMLTable(dict : dict):
+
+def dictionaryToHTMLTable(dict: dict):
     """
     cover dict to html table
     """
-    border = lambda x:f';border: {x}px solid #FED7DD; border-collapse: collapse;text-align: center;padding: 15px'
-    html = HTML(Header = dict.keys(),
+    def border(
+        x): return f';border: {x}px solid #FED7DD; border-collapse: collapse;text-align: center;padding: 15px'
+    html = HTML(Header=dict.keys(),
                 tableStyles={'margin': f'3px auto{border(2)};width: 80%'},
                 trStyles={'background-color': f'#99F2E6{border(1)}'},
-                thStyles={ 'color': f'black{border(1)};font-size:32px'})
+                thStyles={'color': f'black{border(1)};font-size:32px'})
     for i, row in enumerate(zip(*dict.values())):
         # print(row)
-        BGC,TC = ('#FFACBB','DarkGreen') if i%2 == 0 else ('#AAB6FB','Navy')
-        html.addRow(row, trStyles={f'background-color' : f'{BGC}{border(1)};color:{TC};font-size:26px'},
-                        tdStyles={'padding': f'1rem{border(1)};color:{TC};font-size:26px'})
+        BGC, TC = ('#FFACBB', 'DarkGreen') if i % 2 == 0 else (
+            '#AAB6FB', 'Navy')
+        html.addRow(row, trStyles={f'background-color': f'{BGC}{border(1)};color:{TC};font-size:26px'},
+                    tdStyles={'padding': f'1rem{border(1)};color:{TC};font-size:26px'})
     return html
+
 
 if __name__ == '__main__':
     myDict = {
-        'GameName' : ('value11', 'value12', 'value13'),
-        'Skip %' : ['value21', 'value22', 'value23'],
-        'Know %' : ['value31', 'value32', 'value33'],
-        'Fail %' : ['value41', 'value42', 'value43'],
-        'Pass %' : ['value51', 'value52', 'value53'],
-        'Pass+Know' : ['value61', 'value62', 'value63'],
+        'GameName': ('value11', 'value12', 'value13'),
+        'Skip %': ['value21', 'value22', 'value23'],
+        'Know %': ['value31', 'value32', 'value33'],
+        'Fail %': ['value41', 'value42', 'value43'],
+        'Pass %': ['value51', 'value52', 'value53'],
+        'Pass+Know': ['value61', 'value62', 'value63'],
         'Link': ['value71', 'value72', 'value73']
     }
-    loadDescriptions ='''
+    loadDescriptions = '''
     &lt;h1 style="text-align: center">Update time: 2022/09/02 18:44&lt;/h1>
     &lt;table style="margin: 3px auto;border: 2px solid #FED7DD; border-collapse: collapse;text-align: center;padding: 15px;width: 85%;" >
     &lt;tr style="background-color: #99F2E6;border: 1px solid #FED7DD; border-collapse: collapse;text-align: center;padding: 15px;" >
@@ -152,4 +157,3 @@ if __name__ == '__main__':
 
     print(unescape(loadDescriptions))
     print(dictionaryToHTMLTable(myDict))
-
